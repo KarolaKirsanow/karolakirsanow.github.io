57cb1b9e042bed092c43c264806d388a9e1f028f
Karola Kirsanow <karola.kirsanow@gmail.com>
2017-01-16 14:02:33 +0100

    first website commit
    Initial file upload

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+//scroll function modified from levent evemk//
+
+$(document).ready(function(){ 
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('.navbar-default a').removeClass('selected');
+		$(this).addClass('selected');
+    	});
+});
